67b394748b05539dd45aa03add06ce21399b6671
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2025-02-08 23:06:41 +0100

    use torch.jit.trace to optimize model

--- chessbot.py
+++ chessbot.py
@@ -12,9 +12,15 @@ class ChessBot:
         self.model = model
 
         self.model.eval()
+        self.model = self._optimize_model(model)
 
     def search(self, board: chess.Board) -> chess.Move:
         is_maximizing = True if board.turn == chess.WHITE else False
         _, best_move = minimax(board, self.max_depth, -math.inf, math.inf, is_maximizing, self.model, self.transposition_table)
 
         return best_move
+    
+    def _optimize_model(self, model: torch.nn.Module) -> torch.jit.ScriptModule:
+        example_input = torch.zeros(1, 768)
+        traced_model = torch.jit.trace(model, example_input)
+        return traced_model
--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import chess
 import torch
+import time
 
 from modules.nnue import NNUE
 from chessbot import ChessBot
@@ -17,17 +18,23 @@ def main() -> None:
     nnue.eval()
     
     # Create the chessbot object
-    chessbot = ChessBot(max_depth=4, model=nnue, transposition_table=transposition_table)
+    chessbot = ChessBot(max_depth=5, model=nnue, transposition_table=transposition_table)
 
     # Create board and play
     board = chess.Board()
 
     while not board.is_game_over():
+        start = time.time()
+
         move = chessbot.search(board)
+
+        end = time.time()
 
         board.push(move)
 
+        print("----------------------")
         print(board)
+        print(f"Time Elapsed: {end - start:.2f}s")
 
     print(board.result())
 
